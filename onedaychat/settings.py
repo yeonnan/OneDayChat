ddf6735d52c33c0f2f2e17bb772a8407ea9648e7
yeonnan <145152442+yeonnan@users.noreply.github.com>
2025-01-17 22:52:50 +0900

    nginx 추가

--- onedaychat/settings.py
+++ onedaychat/settings.py
@@ -31,7 +31,11 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "43.202.65.24",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
